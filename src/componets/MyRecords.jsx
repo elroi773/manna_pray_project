bf9ac9ac5adb4a878b157a68b4459c9bc1365887
elroi773 <s2457@e-mirim.hs.kr>
2025-07-07 12:15:28 +0900

    사진 정리

--- src/componets/MyRecords.jsx
+++ src/componets/MyRecords.jsx
@@ -4,6 +4,9 @@ import "./MyRecords.css";
 import MyRecord1 from "./img/MyRecord1.png";
 import MyRecord2 from "./img/MyRecord2.png";
 import MyRecord3 from "./img/MyRecord3.png";
+import MyRecord_Mobile1 from "./img/MyRecord_Mobile1.png";
+import MyRecord_Mobile2 from "./img/MyRecord_Mobile2.png";
+import MyRecord_Mobile3 from "./img/MyRecord_Mobile3.png";
 
 const records = [
   { id: "prayer", title: "Prayer topic", subtitle: "나의 기도제목", img: MyRecord1 },
