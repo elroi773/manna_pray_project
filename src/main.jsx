44a2b438cf15ba061c17da9dd861af0980dc79b5
elroi773 <s2457@e-mirim.hs.kr>
2025-07-12 23:39:48 +0900

    구글 로그인 api 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import App from "./App";
 import Login from "./Login/login";
 import Join from "./Join/join";
 
+const clientId = "YOUR_GOOGLE_CLIENT_ID"; // ← 여기에 발급받은 클라이언트 ID 넣기
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/join" element={<Join />} />
-      </Routes>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={clientId}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" element={<Join />} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
