6b85450b334c61dfc5d4e6f64266619e4e084da1
elroi773 <s2457@e-mirim.hs.kr>
2025-07-15 10:31:07 +0900

    오늘의 성경 말씀

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import App from "./App";
 import Login from "./Login/login";
 import Join from "./Join/join";
 import Post from "./Post"; 
+import Today from "./componets/today";
 
 const clientId = "425674337430-rgh226rdlre6qpeb1ojtleb0bufi26go.apps.googleusercontent.com";
 
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route path="/post/:id" element={<Post />} />
           <Route path="/login" element={<Login />} />
           <Route path="/join" element={<Join />} />
+          <Route path="/today" element={<Today />} />
         </Routes>
       </BrowserRouter>
     </GoogleOAuthProvider>
