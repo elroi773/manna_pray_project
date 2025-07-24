e440d8eac6248efd83215ed8af8a7b4042eab94d
elroi773 <s2457@e-mirim.hs.kr>
2025-07-24 10:02:14 +0900

    router 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import CategoryButtons from "../componets/CategoryButtons";
 import CategoryPage from "../pages/CategoryPage";
+import Write from "../Write"; 
 
 export default function AppRouter(){
     return(
@@ -8,6 +9,8 @@ export default function AppRouter(){
             <Routes>
                 <Route path="/" element={<CategoryButtons />}/>
                 <Route path="/category/:categoryName" element={<CategoryPage />} />
+                <Route path="/write" element={<Write loggedInUser="mirim123" />} /> 
+                {/* loggedInUser 나중에 수정해야 함  */}
             </Routes>
         </BrowserRouter>
     );
