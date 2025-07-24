5d24c38f16e4150f33d622596a2b649f4b10000d
elroi773 <s2457@e-mirim.hs.kr>
2025-07-24 09:36:34 +0900

    글쓰기 기능

---
+++ src/componets/WritingButton.jsx
@@ -0,0 +1,19 @@
+//componets/WritingButton.jsx
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./WritingButton.css";
+
+export default function WritingButton() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("../Write.jsx");
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>
+        <img src="https://via.placeholder.com/40" alt="아이콘" />
+      </button>
+    </div>
+  );
+}
