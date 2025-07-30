56b32a98a469be988019914bc933d8fb8b3ea3c5
elroi773 <s2457@e-mirim.hs.kr>
2025-07-30 23:04:49 +0900

    라우트 경로 수정

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom"; // ✅ 추가
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>  {/* ✅ 여기서만 BrowserRouter 사용 */}
+    <BrowserRouter> 
       <App />
     </BrowserRouter>
   </React.StrictMode>
