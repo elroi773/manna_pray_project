3b150b6feecc6f6544160fb43e733b0eeb2e77fd
elroi773 <s2457@e-mirim.hs.kr>
2025-07-30 23:04:43 +0900

    라우트 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-// App.jsx
-import React from "react";
-import AppRouter from "./routes/Router";
+import AppRouter from './routes/Router';
 
 function App() {
   return <AppRouter />;
 }
-
 export default App;
