39e3c3ba373a64111e700e3c08acb08472f24480
elroi773 <s2457@e-mirim.hs.kr>
2025-07-30 23:05:02 +0900

    라우터 경로 수정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,18 +1,25 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import CategoryButtons from "../componets/CategoryButtons";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Home from "../pages/Home";
 import CategoryPage from "../pages/CategoryPage";
-import Write from "../pages/Write"; 
-import Home from "../pages/Home"; 
+import Write from "../pages/Write";
+import CategoryButtons from "../componets/CategoryButtons";
 
-export default function AppRouter(){
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<CategoryButtons />}/>
-                <Route path="/category/:categoryName" element={<CategoryPage />} />
-                <Route path="/write" element={<Write loggedInUser="mirim123" />} />
-                {/* id 는 db 로 다시 해야 함~ */}
-            </Routes>
-        </BrowserRouter>
-    );
-}   
+function Layout() {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
+
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route path="*" element={<Layout />}>
+        <Route index element={<Home />} />               {/* 기본 / 경로 */}
+        <Route path="category/:categoryName" element={<CategoryPage />} />
+        <Route path="write" element={<Write loggedInUser="mirim123" />} />
+      </Route>
+    </Routes>
+  );
+}
