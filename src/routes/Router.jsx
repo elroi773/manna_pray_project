9135f338d6ad41a1992e8705271ddb945e59ea26
elroi773 <s2457@e-mirim.hs.kr>
2025-07-30 22:53:55 +0900

    글쓰기 라우터 경로 문제 해결중

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import CategoryButtons from "../componets/CategoryButtons";
 import CategoryPage from "../pages/CategoryPage";
-import Write from "../Write"; 
+import Write from "../pages/Write"; 
+import Home from "../pages/Home"; 
 
 export default function AppRouter(){
     return(
@@ -9,8 +10,8 @@ export default function AppRouter(){
             <Routes>
                 <Route path="/" element={<CategoryButtons />}/>
                 <Route path="/category/:categoryName" element={<CategoryPage />} />
-                <Route path="/write" element={<Write />} /> 
-                {/* loggedInUser 나중에 수정해야 함  */}
+                <Route path="/write" element={<Write loggedInUser="mirim123" />} />
+                {/* id 는 db 로 다시 해야 함~ */}
             </Routes>
         </BrowserRouter>
     );
