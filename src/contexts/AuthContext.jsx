498d6910cdb0f59bb851cb9a1d9ff9e67160526e
elroi773 <s2457@e-mirim.hs.kr>
2025-08-02 20:21:18 +0900

    글 쓰는거 props 데이터 구글 api 어쩌고

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,28 @@
+// src/contexts/AuthContext.jsx
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user); // user는 null이거나 로그인한 유저 객체
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
