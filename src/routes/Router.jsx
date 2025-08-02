b57890b5cccd9e4203b2d857d25e7cbb65681753
elroi773 <s2457@e-mirim.hs.kr>
2025-08-02 20:21:22 +0900

    글 쓰는거 props 데이터 구글 api 어쩌고

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,26 +2,33 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "../pages/Home";
 import CategoryPage from "../pages/CategoryPage";
 import Write from "../pages/Write";
-import CategoryButtons from "../componets/CategoryButtons";
 
 function Layout() {
   return (
     <div>
+      {/* 공통 컴포넌트 예: <Header />, <CategoryButtons /> 등 넣을 수 있음 */}
       <Outlet />
     </div>
   );
 }
 
 export default function AppRouter() {
-    return (
-      <Routes>
-        {/* 부모 경로에 반드시 path="*" 와일드카드 붙이기 */}
-        <Route path="*" element={<Layout />}>
-          {/* index 경로 (기본 페이지) */}
-          <Route index element={<Home />} />
-          <Route path="category/:categoryName" element={<CategoryPage />} />
-          <Route path="write" element={<Write loggedInUser="mirim123" />} />
-        </Route>
-      </Routes>
-    );
-  }
+  return (
+    <Routes>
+      {/* ✅ 부모는 "/"로, Outlet 포함된 Layout */}
+      <Route path="/" element={<Layout />}>
+        {/* ✅ index ("/") 페이지 */}
+        <Route index element={<Home />} />
+        
+        {/* ✅ "/category/..." 경로 */}
+        <Route path="category/:categoryName" element={<CategoryPage />} />
+        
+        {/* ✅ "/write" 경로 */}
+        <Route path="write" element={<Write loggedInUser="mirim123" />} />
+      </Route>
+
+      {/* ❗ 맨 마지막에 NotFound 처리할 경우만 path="*" 사용 */}
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Routes>
+  );
+}
