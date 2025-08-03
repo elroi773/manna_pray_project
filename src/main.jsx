fa14b1c90c1129666ccfb2399a932f3c209d6a55
elroi773 <s2457@e-mirim.hs.kr>
2025-08-03 22:57:19 +0900

    게시글 쓰는거 에러 수정~

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-// src/main.jsx
+// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -7,26 +7,23 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
 import Login from "./Login/login";
 import Join from "./Join/join";
-import Post from "./Post"; 
+import Post from "./Post";
 import Today from "./componets/today";
-import { AuthProvider } from "./contexts/AuthContext"; // ✅ 추가
 
 const clientId = "425674337430-rgh226rdlre6qpeb1ojtleb0bufi26go.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
-      <AuthProvider> {/* ✅ 여기 추가 */}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/post/:id" element={<Post />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/join" element={<Join />} />
-            <Route path="/today" element={<Today />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/today" element={<Today />} />
+        </Routes>
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
