f4a992faa05a45145de8d1b1dc98ce56b4bec5cc
elroi773 <s2457@e-mirim.hs.kr>
2025-08-05 08:27:29 +0900

    경로 추가

--- src/pages/Mypray.jsx
+++ src/pages/Mypray.jsx
@@ -3,5 +3,9 @@ import { useNavigate } from "react-router-dom";
 import "./Mypray.css";
 
 export default function Mypray(){
-    
+    return(
+        <>
+            <h1>나의 기도제목 기록 페이지 입니다</h1>
+        </>
+    )
 }
