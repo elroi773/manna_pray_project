afd6bfca3ff704ad282d0b430ba76322050b96a6
elroi773 <s2457@e-mirim.hs.kr>
2025-08-05 08:27:26 +0900

    경로 추가

---
+++ src/pages/MyContemplation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Contemplation.css";
+
+export default function MyContemplation(){
+    return(
+        <>
+            <h1>나의 묵상 기록 페이지 입니다</h1>
+        </>
+    )
+}
