24f750cf4371a1a085e9a1d79593b9151ab11a9a
elroi773 <s2457@e-mirim.hs.kr>
2025-08-07 23:50:40 +0900

    컴포넌트 postlist 불러오기

--- src/pages/MyContemplation.jsx
+++ src/pages/MyContemplation.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PostList from "../componets/PostList";
 import "./MyContemplation.css";
 
 const youtubeVideos = [
@@ -7,52 +8,42 @@ const youtubeVideos = [
     src: "https://www.youtube.com/embed/Gi5pEhruJQ8",
     title: "다시 일어서는 감사",
     myReflection: "하나님의 은혜로 다시 일어서는 힘을 얻었습니다. 감사함으로 하루를 살아가겠습니다.",
-    others: [
-      "나도 오늘 말씀 통해 위로받았어요. - 사용자A",
-      "기도하며 감사하는 삶을 살아야겠네요. - 사용자B",
-    ],
   },
   {
     day: "2일차",
     src: null,
     title: "coming soon",
     myReflection: "",
-    others: [],
   },
   {
     day: "3일차",
     src: null,
     title: "coming soon",
     myReflection: "",
-    others: [],
   },
   {
     day: "4일차",
     src: null,
     title: "coming soon",
     myReflection: "",
-    others: [],
   },
   {
     day: "5일차",
     src: null,
     title: "coming soon",
     myReflection: "",
-    others: [],
   },
   {
     day: "6일차",
     src: null,
     title: "coming soon",
     myReflection: "",
-    others: [],
   },
   {
     day: "7일차",
     src: null,
     title: "coming soon",
     myReflection: "",
-    others: [],
   },
 ];
 
@@ -99,15 +90,7 @@ export default function MeditationRecords() {
           )}
 
           <h3>다른 사람들의 묵상글</h3>
-          {youtubeVideos[selectedDay].others.length > 0 ? (
-            <ul className="others-list">
-              {youtubeVideos[selectedDay].others.map((entry, idx) => (
-                <li key={idx}>{entry}</li>
-              ))}
-            </ul>
-          ) : (
-            <p className="others-empty">등록된 묵상글이 없습니다.</p>
-          )}
+          <PostList />
         </div>
       )}
     </div>
