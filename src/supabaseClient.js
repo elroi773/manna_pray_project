3383239f57ec9cf5dcbb38429eb6dc39611af9e3
elroi773 <s2457@e-mirim.hs.kr>
2025-08-13 00:02:16 +0900

    supabase 로 수정

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,13 @@
 // src/supabaseClient.js
+//key 
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpwb3ZmZ3Jxbmxiam90ZHJ2ZHlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5NjU2MDAsImV4cCI6MjA3MDU0MTYwMH0.Tw8JKMJ-g5tyxApGApdryoIzK-IpUuTVTSCLvpZDh14
+//url 
+//https://zpovfgrqnlbjotdrvdya.supabase.co
+
+
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://manna-pray-project.supabase.co' // Supabase 프로젝트 URL
-const supabaseKey = 'sb_publishable_sqcLcmJroymr_fUdVKFO2g_6zDDOUnH' // Publishable key
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = 'https://zpovfgrqnlbjotdrvdya.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpwb3ZmZ3Jxbmxiam90ZHJ2ZHlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5NjU2MDAsImV4cCI6MjA3MDU0MTYwMH0.Tw8JKMJ-g5tyxApGApdryoIzK-IpUuTVTSCLvpZDh14'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)  // 여기 변수명을 맞춰주세요!
